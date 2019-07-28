11d65ef86e78e5704e22df6d88425931a989e458
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-28 19:45:37 +0530

    Create s52.py

---
+++ s52.py
@@ -0,0 +1,13 @@
+import sys,string
+def maxOfSegmentMinimum(Lacs, necs, kins):
+    if kins == 1:
+        return min(Lacs)
+    if kins == 2 :
+        return max(Lacs[0], Lacs[necs - 1])
+    return max(Lacs)
+
+necs,kins = map(int,input().split())
+Lacs = [ int(x) for x in input().split()]
+necs = len(Lacs)
+ans = maxOfSegmentMins(Lacs, necs, kins)
+print(ans)
