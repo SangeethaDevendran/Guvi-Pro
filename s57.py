79e353b29c1bec2d8cc7d7b787873e660e853558
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 20:14:49 +0530

    Update s57.py

--- s57.py
+++ s57.py
@@ -1,10 +1,18 @@
-cha,cho=map(str,input().split())
-counts=0
-for i in range(0,len(cha)):
-    for j in range(0,len(cho)):
-        if cha[i]==cho[j]:
-            counts+=1
-if counts>=2:
-    print("yes")
-else:
-    print("no")
+ssa=input().split()
+g3=int(ssa[1])
+m3=int(ssa[0])
+ssa=[int(d) for d in ssa[0]]
+h3=0
+c3=1;
+while h3==0:
+    prod=c3*m3
+    k=[int(d) for d in str(prod)]
+    r=0
+    j=len(k)-1
+    while k[j]==0:
+        r+=1
+        j-=1
+    if r>=g3:
+        print(prod)
+        h3=1
+    c3+=1
