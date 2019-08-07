b6632b7a0f944f56ae3a005d84dc978cc46893b6
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 20:10:07 +0530

    Update s56.py

--- s56.py
+++ s56.py
@@ -1,10 +1,13 @@
-chaal,choal=map(int,input().split())
-lists=list(map(int,input().split()))
-caa=0
-for i in lists:
-    kaa=86400-i
-    choal-=kaa
-    caa+=1
-    if choal<=0:
-        break  
-print(caa)
+ppa=int(input())
+qqa=list(map(int,input().split()))
+a3a=0
+b3a=0
+qqa.sort(reverse=True)
+for i in qqa:
+  qqa=a3a+i
+  if b3a>qqa:
+    a3a=qqa
+  else:
+    a3a=b3a
+    b3a=qqa
+print(a3a,b3a)
