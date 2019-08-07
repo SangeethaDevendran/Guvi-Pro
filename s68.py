58329701eda05586a41549813b93a44638fd664e
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 21:09:39 +0530

    Update s68.py

--- s68.py
+++ s68.py
@@ -1,5 +1,14 @@
-cha,cho=map(int,input().split())
-if cho==1:
-   print(1,2)
-else:
-   print(1,cha-cho)
+nt1=int(input())
+liata=[]
+at=nt1//2+nt1
+for i in range(1,nt1+1):
+    if i%2==0:
+        liata.append(i)
+for i in range(1,nt1+1):
+    if i%2!=0:
+        liata.append(i)
+for i in range(1,nt1+1):
+    if i%2==0:
+        liata.append(i)
+print(at)
+print(*liata)
