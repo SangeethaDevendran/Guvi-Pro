d8652cc764fe1155e8cc72bf8c0e42344efd9b49
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 20:45:08 +0530

    Update s63.py

--- s63.py
+++ s63.py
@@ -1,8 +1,10 @@
-cha=input()
-cho=[]
-for i in cha:
-  if i not in cho:
-    cho.append(i)
-  else:
-    break  
-print(len(cho))
+import sys, string, math
+sisa = input()
+sisa = sisa.lower()
+sont = string.ascii_lowercase
+
+for c in sont :
+    if c not in sisa :
+        print('no')
+        sys.exit()
+print('yes')
