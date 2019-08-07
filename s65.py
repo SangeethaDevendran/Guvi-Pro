8c8398dfbc3f6da234abbc2f4d51d484ba77ae94
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 20:49:42 +0530

    Update s65.py

--- s65.py
+++ s65.py
@@ -1,8 +1,21 @@
-cha,cho=map(int,input().split())
-lists=list(map(int,input().split()))[:cha]
-raa=int(input())
-saa=(sum(lists)-lists[cho])//2
-if (saa==raa):
-    print("Bon Appetit")
-else:
-    print(abs(saa-raa))
+tvs,sunt = map(int,input().split())
+veeta = list(map(int,input().split()))
+bht,neet = 0,[]
+for i in range(0,len(veeta)):
+  tvs= i
+  for p in range(0,len(veeta)):
+    for l in range(0,sunt):
+      if l == sunt-1:
+        try:
+          bht += veeta[p+i]
+        except IndexError:
+            tvs = tvs-1
+            bht +=veeta[tvs]
+      else:
+        bht += veeta[i]
+    neet.append(bht)
+    bht = 0
+for i in range(0,len(veeta),sunt):
+  bht = sum(veeta[i:i+sunt])
+  neet.append(bht)
+print(*sorted(set(neet)))
