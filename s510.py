19216d7ac68ba9d6e4ef1314925059a9ceac6c14
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-08-07 20:26:39 +0530

    Update s510.py

--- s510.py
+++ s510.py
@@ -1,11 +1,25 @@
-cha=int(input())
-cho,che=3,3
-while cha > 0:
-    if cho==0:
-        che*=2
-        cho=che
-    if cha==1:
-        print(cho)
-        break
-    cha-=1
-    cho-=1
+xt1,yta=map(int,input().split())
+zt=[]
+
+for i in range(yta):
+  zt.append(list(map(int,input().split())))
+maht=10000000
+bht=0
+
+for i in range(yta):
+  if zt[i][0]==1:
+    sont=zt[i][1]
+    pict=zt[i][2]
+    for j in range(i+1,yta):
+      if zt[j][0]==sont:
+        sont=zt[j][1]
+        pict=pict+zt[j][2]
+    if pict<maht and sont==xt1:
+      maht=pict
+      bht=bht+1
+
+if bht==0:
+  print(-1)
+else:
+  print(maht)
+© 2019 Git
